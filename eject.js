0e93b4a688aa1cbf32da6e65800836e36823bc00
develon2015 <302615249@qq.com>
2020-12-03 11:17:56 +0800

    --create and --init options

--- eject.js
+++ eject.js
@@ -4,6 +4,8 @@ const fs = require('fs');
 const DIR_NDS = __dirname;
 const DIR_NDS_MODULES = path.resolve(DIR_NDS, 'node_modules');
 
+let _project = '.';
+
 function eject$babel_config_json() {
     const babel_config_json = {
         "plugins": [
@@ -23,24 +25,25 @@ function eject$babel_config_json() {
             ]
         ]
     };
-    fs.writeFileSync(path.resolve(process.cwd(), 'babel.config.json'), JSON.stringify(babel_config_json, null, 2));
+    fs.writeFileSync(path.resolve(_project, 'babel.config.json'), JSON.stringify(babel_config_json, null, 2));
 }
 
 function eject$webpack_config_js() {
     const filename = 'webpack.config.js';
     const webpack_config_js = fs.readFileSync(path.resolve(__dirname, './public/webpack.config.js')).toString('utf-8');
     const replace = webpack_config_js.replace('`@BABEL_LOADER`', JSON.stringify(`${DIR_NDS_MODULES}${path.sep}babel-loader`));
-    fs.writeFileSync(path.resolve(process.cwd(), filename), replace);
+    fs.writeFileSync(path.resolve(_project, filename), replace);
 }
 
 function eject$tsconfig_json() {
     const filename = 'tsconfig.json';
     const webpack_config_js = fs.readFileSync(path.resolve(__dirname, './public/tsconfig.json.template')).toString('utf-8');
     const replace = webpack_config_js.replace('`@BABEL_LOADER`', JSON.stringify(`${DIR_NDS_MODULES}${path.sep}babel-loader`));
-    fs.writeFileSync(path.resolve(process.cwd(), filename), replace);
+    fs.writeFileSync(path.resolve(_project, filename), replace);
 }
 
-function eject() {
+function eject(project = process.cwd()) {
+    _project = project;
     console.info('弹出配置文件！');
     eject$babel_config_json();
     eject$webpack_config_js();
--- index.js
+++ index.js
@@ -1,13 +1,58 @@
+const path = require('path');
+const fs = require('fs');
+
 const nds = require('./node-dev-server');
 const eject = require('./eject');
 const { consoleHook, _log, } = require('./console-hook');
 
+/**
+ * Create a project dir and init it
+ * @param {string} project 
+ */
+function create(project) {
+    console.info(`Create project ${project}`);
+    fs.mkdirSync(project); // dir: pro
+    init(project);
+}
+
+/**
+ * Init a project, e.g: eject the TypeScript config file.
+ * @param {string} project 
+ */
+function init(project) {
+    console.info(`Init project ${project}`);
+    const DIR_SRC = path.resolve(project, 'src');
+    fs.mkdirSync(DIR_SRC); // dir: pro/src
+    fs.writeFileSync(path.resolve(DIR_SRC, 'index.ts'), `console.log('Hello, TypeScript!');\n`); // file: pro/src/index.ts
+    eject(project);
+    console.log(`Project init succeed! To continue, please:\n\ncd ${project}\nyarn init -y\nyarn add @types/node`);
+}
+
 function main() {
     switch (process.argv[2]) {
+        case '-i':
+        case 'init':
+        case '--init': { // Init cwd() or a project
+            consoleHook();
+            init(path.resolve(process.cwd(), process.argv[3] || '.'));
+            break;
+        }
+        case '-c':
+        case 'create':
+        case '--create': { // Create a project directory and init
+            if (!process.argv[3]) { // No a project name provided
+                help();
+                break;
+            }
+            consoleHook();
+            create(path.resolve(process.cwd(), process.argv[3]));
+            break;
+        }
         case '-ej':
+        case 'eject':
         case '--eject': {
             consoleHook();
-            eject();
+            eject(path.resolve(process.cwd(), process.argv[3] || '.'));
             break;
         }
         case '-h':
@@ -26,16 +71,19 @@ function main() {
 
 function help() {
     const info = `
-Usage: nds [project | options]
-       node-dev-server [project | options]
+Usage: nds [options] [project]
+       node-dev-server [options] [project]
 
        Serves a Node.js app. Restart the app on changes.
     
        Options:
-           [project]            project directory
-           -ej, --eject         eject the TypeScript supported configuration file
-           -h, --help           display help for command`;
+           [project]            The project directory
+           -c, --create         Create a TypeScript project and init it
+           -i, --init           Init a TypeScript project
+           -ej, --eject         Eject the TypeScript supported configuration file
+           -h, --help           Display help for command`;
     _log(info);
+    process.exit(0);
 }
 
 main();
